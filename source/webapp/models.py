a9b6af3b83ff01a2f2326b33febd01f4b1209b02
Emil <emilrakaev@gmail.com>
2020-07-23 00:21:14 +0600

    Edit field datetime

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Goal(models.Model):
     describe = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Description')
     status = models.CharField(max_length=40, null=False, blank=False, default='new', verbose_name='Status')
-    execute_at = models.DateTimeField(auto_now_add=True, verbose_name='Date of execution')
+    execute_at = models.DateField(null=True, blank=True, default=None, verbose_name='Date of execution')
 
     def __str__(self):
-        return "{}. {}. {}. {}".format(self.pk, self.describe, self.status, self.execute_at)
+        return "{}, {}, {}, {}".format(self.pk, self.describe, self.status, self.execute_at)
 
     class Meta():
         verbose_name = 'Goal'
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from webapp.models import Goal
 
+
 def index_view(request):
     data = Goal.objects.all()
     return render(request, 'index.html', context={
