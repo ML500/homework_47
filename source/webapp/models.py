64ea1c6fde1fdfcfd3823a99447f86a742906c7c
Emil <emilrakaev@gmail.com>
2020-07-22 16:24:44 +0600

    Add models + migrations + css file

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    describe = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Description')
+    status = models.CharField(max_length=40, null=False, blank=False, default='new', verbose_name='Status')
+    execute_at = models.DateTimeField(auto_now_add=True, verbose_name='Date of execution')
+
+    def __str__(self):
+        return "{}. {}. {}. {}".format(self.pk, self.describe, self.status, self.execute_at)
+
+    class Meta():
+        verbose_name = 'Goal'
+        verbose_name_plural = 'Goals'
