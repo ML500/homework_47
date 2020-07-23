8ac7fa31b4bc4e2ed884a078da9365f7b2607da8
Emil <emilrakaev@gmail.com>
2020-07-23 15:10:37 +0600

    edit css and all templates

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -26,7 +26,10 @@ def goal_create_view(request):
 
 def delete_goal(request):
     if request.method == 'GET':
-        return render(request, 'delete_form.html')
+        data = Goal.objects.all()
+        return render(request, 'delete_form.html', context={
+        'goals': data
+    })
     elif request.method == 'POST':
         id_goal = request.POST.get('id')
         goal = Goal.objects.get(pk=id_goal)
