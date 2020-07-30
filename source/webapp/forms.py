b41ebed7e3acc3cad119585d0a96f08c058e1f54
Emil <emilrakaev@gmail.com>
2020-07-30 15:22:06 +0600

    Edit format of date in output

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -7,6 +7,6 @@ default_status = STATUS_CHOICES[0][0]
 class GoalForm(forms.Form):
     describe = forms.CharField(max_length=200, required=True, label='Description')
     detail = forms.CharField(max_length=3000, required=True, label='Detail', widget=forms.Textarea)
-    execute_at = forms.DateField(required=True, label='Date of execution',
+    execute_at = forms.DateField(required=False, label='Date of execution',
                                  widget=forms.DateInput(attrs={'type': 'date'}))
     status = forms.ChoiceField(choices=STATUS_CHOICES, initial=default_status, label='Moderation')
